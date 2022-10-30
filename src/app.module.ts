b4f766dd9666bc6d37ffd1c8e28c632aab68efaa
Raj Kinkhabwala <kinkhabwalaraj@gmail.com>
2022-10-30 01:32:14 -0700

    added configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, ConfigModule.forRoot({
+    envFilePath: [".env.development"],
+    load: [configuration]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
